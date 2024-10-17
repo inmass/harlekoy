03c9681f7f2d2f1362e1c6e556c58fd01ada61c9
iinmass <inmass.idbel@gmail.com>
2024-10-18 00:07:00 +0100

    add logic to process updated users

--- app/Services/UserService.php
+++ app/Services/UserService.php
@@ -9,10 +9,24 @@ use Illuminate\Support\Facades\Cache;
 class UserService
 {
     const CACHE_KEY = 'users_to_update';
-    const CACHE_TTL = 30;
 
     public static function queueUserForProcessing(User $user)
     {
-        //
+        $key = self::CACHE_KEY;
+        self::createCacheIfNotExists($key);
+        
+        $users = Cache::get($key);
+        // we want to make sure we send the latest data to the queue
+        $users = $users->keyBy('id');
+        $users[$user->id] = $user;
+        
+        Cache::put($key, $users->values());
+    }
+
+    private static function createCacheIfNotExists(string $key)
+    {
+        if (!Cache::has($key)) {
+            Cache::forever($key, collect());
+        }
     }
 }
