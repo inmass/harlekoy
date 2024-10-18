f1266beb1ef01ebb11871e9f1e90cc0d81d5ec1a
iinmass <inmass.idbel@gmail.com>
2024-10-18 14:25:02 +0100

    processing updated users every 2 mins instead of 5

--- routes/console.php
+++ routes/console.php
@@ -9,4 +9,4 @@ Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
 
-Schedule::command(ProcessUpdatedUsers::class)->everyFiveMinutes();
+Schedule::command(ProcessUpdatedUsers::class)->everyTwoMinutes();
